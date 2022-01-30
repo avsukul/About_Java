b528f88afacdfab919ee501b93c565528ccaaf4d
Victor <24920959+avsukul@users.noreply.github.com>
2022-01-30 13:23:31 -0500

    adding LinkList

---
+++ Algorithms/src/Lists/LinkList/EmployeeNode.java
@@ -0,0 +1,30 @@
+package Lists.LinkList;
+
+public class EmployeeNode {
+    private Employee employee;
+    private EmployeeNode next;  // reference to the next node
+
+    public EmployeeNode(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    public String toString() {
+        return employee.toString();
+    }
+}
