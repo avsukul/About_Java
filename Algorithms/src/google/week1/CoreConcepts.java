fea5e09ede0f3abf40d212916f819ae728f04435
jsukulv <24920959+avsukul@users.noreply.github.com>
2022-11-29 17:27:23 -0500

    more arrays fundamental

--- Algorithms/src/google/week1/CoreConcepts.java
+++ Algorithms/src/google/week1/CoreConcepts.java
@@ -4,6 +4,10 @@ package google.week1;
 // import statement shows which other files this file is using.
 // only to be used if not in the same package.
 
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CoreConcepts { // type declaration
 
    boolean aBoolean = true; // true or false
@@ -95,7 +99,7 @@ class MathOperators {
 
 }
 
-class Arrays {
+class Arrayz {
 
     // valid array declarations
     int[] intArrays = new int[5];
@@ -117,9 +121,102 @@ class Arrays {
         int length = arr.length; // 3
 
         // iterate through an array with for loop & for-each loop
-        iterate(arr);
+//        iterate(arr);
+
+        // insert elements into an array
+//        arr[0] = 5;
+        // remove the element above
+//        arr[0] = 0;
+
+        // min value in array
+        findMinValue(arr);
+        // System.out.print(findMinValue(arr));
+
+        // max value in array
+        findMaxValue(arr);
+        // System.out.print(findMaxValue(arr));
+
+        // copying an array into another array
+        // iteration
+        int[] ints = new int[arr.length];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = arr[i];
+        }
+        // Arrays.copyOf
+        int[] newArr = Arrays.copyOf(arr, 10);
+//        iterate(newArr);
+        // Arrays.copyOfRange
+        int[] newerArr = Arrays.copyOfRange(arr, 0, arr.length);
+//        iterate(newerArr);
+
+        // sort an Array
+        int[] a  = {5,3,2,1,5,45,634,2311,21};
+        Arrays.sort(a);
+//        iterate(a);
+
+        // sort objects
+        Employee[] emp = new Employee[3];
+        emp[1] = new Employee("bob".toUpperCase(), 2332);
+        emp[0] = new Employee("Tom".toUpperCase(), 234);
+        emp[2]= new Employee("John".toUpperCase(), 234324);
+        Arrays.sort(emp, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                return e1.name.compareTo(e2.name);
+            }
+        });
+        for(int i=0; i < emp.length; i++) {
+//            System.out.println(emp[i].name);
+        }
+
+        // Arrays.fill()
+        int[] intArray = new int[10];
+
+        Arrays.fill(intArray, 123);
+
+        int[] aa = new int[10];
+
+        Arrays.fill(aa, 3, 5, 123) ;
+//        System.out.println(Arrays.toString(aa));
+
+        // Arrays.binarySearch but you MUST Sort the array
+        int[] search = {3,43,4,3,2,23,21,4,5};
+        Arrays.sort(search);
+//        System.out.println(Arrays.toString(search));
+        int index = Arrays.binarySearch(search, 3);
+//        System.out.println(index);
+
+        // search part of the array
+        int[] ar = {0,2,4,6,8,10};
+        int in = Arrays.binarySearch(ar, 0, 4, 2);
+//        System.out.println(in);
+
+        // check if arrays are equal
+        int[] ints1 = {0,2,4,6,8,10};
+        int[] ints2 = {0,2,4,6,8,10};
+        int[] ints3 = {10,8,6,4,2,0};
+
+        boolean ints1EqualsInts2 = Arrays.equals(ints1, ints2);
+        boolean ints1EqualsInts3 = Arrays.equals(ints1, ints3);
+
+        System.out.println(ints1EqualsInts2);
+        System.out.println(ints1EqualsInts3);
+
+
+
+
+
     }
 
+    private static class Employee {
+        public String name;
+        public int employeeId;
+
+        public Employee(String name, int employeeId){
+            this.name       = name;
+            this.employeeId = employeeId;
+        }
+    }
     static void iterate(int[] arr) {
         // for loop
         for (int i = 0; i < arr.length; i++) {
@@ -129,6 +226,26 @@ class Arrays {
         for (int i : arr) {
             System.out.println(i);
         }
+    }
+
+    static int findMinValue(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    static int findMaxValue(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 
     private static double computeVolume(int[] ints) {
